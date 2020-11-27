943c870216d4e9b6b62806e8177cdba0761926f0
Ranu28 <59174008+Ranu28@users.noreply.github.com>
2020-11-27 08:53:09 +0530

    Login page and few other

---
+++ UniversityCanteen/src/java/sql.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java;
+
+import com.mysql.jdbc.Connection;
+
+/**
+ *
+ * @author ranup
+ */
+public class sql {
+
+    public static class DriverManager {
+
+        public static Connection getConnection(String jdbcmysqllocalhost3308universitycanteen, String root, String string) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public DriverManager() {
+        }
+    }
+    
+}
---
+++ UniversityCanteen/src/universitycanteen/DBconnect.java
@@ -0,0 +1,28 @@
+
+package universitycanteen;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
+
+public class DBconnect {
+    
+    public static Connection connect(){
+        
+        Connection con = null;
+        
+        try{
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3308/universitycanteen","root","");
+            
+        }catch(Exception e){
+             
+        }
+        
+       
+        
+        return con;        
+    }
+    
+}
